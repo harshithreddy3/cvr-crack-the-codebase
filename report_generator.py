137ced1946af2de423efcd6a4e4eeaf39f264967
harshithreddy3 <harshith3691@gmail.com>
2026-03-13 11:11:22 +0530

    Update report_generator.py

--- report_generator.py
+++ report_generator.py
@@ -2,5 +2,5 @@
 def write_output(metrics_dict):
     """Writes the final analytics dictionary to a text file for HR."""
     out_file = open("hr_report.txt", "w")
-    out_file.write(metrics_dict)
+    out_file.write(str(metrics_dict))
     print("Report saved to hr_report.txt successfully!")
